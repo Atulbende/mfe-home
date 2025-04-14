b5adf06311b81366e9676d2027be0c398de2ccd2
Atulbende <107322576+Atulbende@users.noreply.github.com>
2025-04-14 10:31:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-xl font-bold">Welcome to Home Page</h1>
+      <p>Start shopping your favorite items!</p>
+    </div>
+  );
+}
+export default App;
